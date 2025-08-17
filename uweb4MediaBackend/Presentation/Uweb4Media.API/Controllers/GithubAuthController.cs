519bec91756d87e06860bb200cc2bf1008c1e8c1
yusuf <96827425+yusufaaras@users.noreply.github.com>
2025-08-17 17:54:15 +0300

    .
    .

--- uweb4MediaBackend/Presentation/Uweb4Media.API/Controllers/GithubAuthController.cs
+++ uweb4MediaBackend/Presentation/Uweb4Media.API/Controllers/GithubAuthController.cs
@@ -72,7 +72,7 @@ public class GitHubAuthController : ControllerBase
             GoogleId = user.GoogleId 
         }; 
         var token = JwtTokenGenerator.GenerateToken(appUserResult); 
-        Console.WriteLine("[DEBUG] Redirect URL:https://primeweb4-9c444.firebaseapp.com/auth/github-success?token=" + token.Token);
+        Console.WriteLine("[DEBUG] Redirect URL:https://primeweb4-9c444.firebaseapp.com/#/auth/github-success?token=" + token.Token);
         return Redirect($"https://primeweb4-9c444.firebaseapp.com/#/auth/github-success?token={token.Token}");
         
     }
