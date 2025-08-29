59963f88d4b57c8ed5da754149916f3a2055a0c1
yusuf <96827425+yusufaaras@users.noreply.github.com>
2025-08-29 12:08:53 +0300

    .
    .

--- uweb4MediaBackend/Core/uweb4Media.Application/obj/Debug/net8.0/uweb4Media.Application.AssemblyInfo.cs
+++ uweb4MediaBackend/Core/uweb4Media.Application/obj/Debug/net8.0/uweb4Media.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uweb4Media.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a84887563a6ed10c903276cb72176e5e4670d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3f68242a0205a7eb1e27ddb71f02fd5c5bb21e")]
 [assembly: System.Reflection.AssemblyProductAttribute("uweb4Media.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uweb4Media.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
